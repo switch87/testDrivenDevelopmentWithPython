9fa5b4d0ac08e854c133e6a38ea3ee4de3be756c
switch87 <gert@gepe-biljarts.be>
2015-08-24 12:01:11 +0200

    Add gunicorn to virtualenv requirements

--- superlists/settings.py
+++ superlists/settings.py
@@ -24,6 +24,8 @@ SECRET_KEY = 'm_*#^bd@^6=#_1p#72yccx%%!_)@$r72uc9v0#(3j=k*t_6&id'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
+# Needed when DEBUG=False
 ALLOWED_HOSTS = []
 
 
