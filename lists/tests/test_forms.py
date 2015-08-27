c40c95bee384ecc3695b68b6f8cd56db84488006
switch87 <gert@gepe-biljarts.be>
2015-08-27 15:00:54 +0200

    use new form in home_page, simplify tests. NB breaks stuff

---
+++ lists/tests/test_forms.py
@@ -0,0 +1,7 @@
+from lists.forms import ItemForm, EMPTY_LIST_ERROR
+
+
+def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
