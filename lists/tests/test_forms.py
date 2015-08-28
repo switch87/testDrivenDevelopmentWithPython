b1e055709bb017ec81d267240b00ef137150059f
switch87 <gert@gepe-biljarts.be>
2015-08-28 16:02:52 +0200

    Lets go to chapter 12!

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,7 +1,18 @@
+from django.test import TestCase
 from lists.forms import ItemForm, EMPTY_LIST_ERROR
+from lists.models import List, Item
 
 
-def test_form_validation_for_blank_items(self):
+class FormTest(TestCase):
+    def test_form_validation_for_blank_items(self):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
